ba03fb916f7273b8faa5d53ee2319b1e32d367cc
KetanRathor <93254444+KetanRathor@users.noreply.github.com>
2024-03-08 16:26:43 +0530

    Create ClimbingStairs.js

---
+++ ClimbingStairs.js
@@ -0,0 +1,16 @@
+function climbStairs(n) {
+    if (n <= 2) {
+        return n;
+    }
+
+    let prev1 = 1;
+    let prev2 = 2;
+
+    for (let i = 3; i <= n; i++) {
+        const current = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = current;
+    }
+
+    return prev2;
+}
