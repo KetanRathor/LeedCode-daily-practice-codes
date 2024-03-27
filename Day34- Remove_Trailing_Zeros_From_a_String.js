f5a8d3fa7fe2e4a39491b40cc21ad38cef3b96c9
KetanRathor <93254444+KetanRathor@users.noreply.github.com>
2024-03-27 06:06:43 +0000

    Create Day34- Remove_Trailing_Zeros_From_a_String.js

---
+++ Day34- Remove_Trailing_Zeros_From_a_String.js
@@ -0,0 +1,12 @@
+var removeTrailingZeros = function(num) {
+    for(let i=num.length-1; i>=0; i--){
+        if(num[i]!=="0"){
+            break;
+        }
+        else {
+           num = num.slice(0,i)
+        }
+    }
+    return num;
+    
+};
